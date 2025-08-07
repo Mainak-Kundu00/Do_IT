b960ce5cec16f6a26497cefa0a87e84e4377ffd1
Mainak-Kundu00 <smnath2000@gmail.com>
2025-08-07 13:31:29 +0530

    task item class add

--- todo.js
+++ todo.js
@@ -22,12 +22,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     task_container.addEventListener("click", (e) => {
+        //clicked on the checkbox
+        const div = e.target.parentElement;
+        const divId = e.target.parentElement.getAttribute("item_id");
         if(e.target.matches("INPUT[TYPE=CHECKBOX]")){
-            // console.log(e.target.parentElement);
+            // console.log(e.target.parentElement.id);
 
         }
+        // task is checked by the user
         if(e.target.checked) {
-            // console.log("hi");
+
+        }
+        else{
+            console.log("not hi");
+            
         }
         
         
@@ -74,17 +82,23 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function renderTask(item){
+        //creating an item
         const div = document.createElement("div");
+        //creating the tailwind classes
+        const taskClasses = "bg-white shadow-sm rounded-md px-4 py-2 mb-2 border border-gray-200 hover:bg-gray-300 transition-all duration-200";
+        
+        div.classList.add(...taskClasses.split(" "));
         div.setAttribute('item_id', item.id);
         div.innerHTML = `
             <input type="checkbox" name="completed">
-            <span>${item.text}</span>
-            <button class="float-right text-red-500"> 
+            <span class="ml-1.5">${item.text}</span>
+            <button class="float-right text-red-500 hover:text-red-700"> 
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
                 </svg>
             </button>
             `;
+        //adding item to the task container and updating the counter
         task_container.appendChild(div);
         count.innerHTML = `${tasks.length} items` ;
 
